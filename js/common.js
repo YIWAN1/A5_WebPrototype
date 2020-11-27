d02296e428aa2aef9518ee037c8ca097baf197a0
YIWAN <ywan5525@uni.sydney.edu.au>
2020-11-27 04:50:14 +0000

    iteration & responsive &&navi change

--- js/common.js
+++ js/common.js
@@ -35,9 +35,47 @@ $(function() {
 })
 
 $(function() {
+    $(".header ul li:first-child").on("touchstart ", function() {
+        // if ($(this).children("div").css("display") == "none") {
+        //     $(this).children("div").slideDown()
+        // } else if ($(this).children("div").css("display") == "block") {
+        //     $(this).children("div").slideUp()
+        // }
+        $(this).children("div").slideToggle()
+    })
+})
+
+$(function() {
     $(".header ul li:first-child").mouseenter(function() {
         $(this).children("div").stop(true, true).slideDown()
     }).mouseleave(function() {
         $(this).children("div").stop(true, true).slideUp()
     })
+})
+
+$(function() {
+    $(".menuBox").click(function() {
+        $(".header ul").slideToggle(function() {
+            if ($(".header ul").css("display") == "block") {
+                $(".menuBox").attr("src", "images/mp_delete.png")
+            } else if ($(".header ul").css("display") == "none") {
+                $(".menuBox").attr("src", "images/mp_menu.png")
+            }
+        })
+
+
+    })
+})
+
+$(window).resize(function() {
+    if ($(this).width() > 992) {
+        $(".header ul").css({
+            "display": "flex"
+        })
+    } else {
+        $(".header ul").css({
+            "display": "none"
+        })
+        $(".menuBox").attr("src", "images/mp_menu.png")
+    }
 })
